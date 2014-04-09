574f64feb4dafba37e7b27de4c81af310cc3d357
Mondain <mondain@gmail.com>
2014-04-09 16:52:51 -0700

    Debugging daemon setups

--- src/main/daemon/install-service.bat
+++ src/main/daemon/install-service.bat
@@ -1,10 +1,93 @@
 @echo off
+rem Licensed to the Apache Software Foundation (ASF) under one or more
+rem contributor license agreements.  See the NOTICE file distributed with
+rem this work for additional information regarding copyright ownership.
+rem The ASF licenses this file to You under the Apache License, Version 2.0
+rem (the "License"); you may not use this file except in compliance with
+rem the License.  You may obtain a copy of the License at
+rem
+rem     http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.
+
+rem ---------------------------------------------------------------------------
+rem Windows Service Install script // jvm version
+rem
+rem ---------------------------------------------------------------------------
 
 SETLOCAL
 
 if NOT DEFINED RED5_HOME set RED5_HOME=%~dp0
 
-echo Installing Red5 service
-prunsrv //IS//REDFIVE --DisplayName="Red5" --Description="Red5 Media Server" --StartPath="%RED5_HOME%" --Type=interactive --LogLevel=DEBUG --LogPath="%RED5_HOME%\log" --LogPrefix=procrun.log --Startup=auto --StdOutput="%RED5_HOME%\log\stdout.log" --StdError="%RED5_HOME%\log\stderr.log" --Classpath="%RED5_HOME%\red5-server-bootstrap.jar;%RED5_HOME%\conf" --StartMode=jvm --StartClass=org.red5.server.Bootstrap --StopMode=jvm --StopClass=org.red5.server.Shutdown --StopParams=9999;red5user;changeme ++JvmOptions=-Djavax.net.ssl.keyStore="%RED5_HOME%/conf/keystore.jmx" ++JvmOptions=-Djavax.net.ssl.keyStorePassword=password ++JvmOptions=-Xverify:none ++JvmOptions=-XX:+TieredCompilation ++JvmOptions=-XX:+UseBiasedLocking ++JvmOptions=-XX:+UseStringCache ++JvmOptions=-XX:+UseParNewGC ++JvmOptions=-XX:InitialCodeCacheSize=8m ++JvmOptions=-XX:ReservedCodeCacheSize=32m ++JvmOptions=-Dorg.terracotta.quartz.skipUpdateCheck=true ++JvmOptions=-Dlogback.ContextSelector=org.red5.logging.LoggingContextSelector ++JvmOptions=-Dcatalina.useNaming=true ++JvmOptions=-Djava.security.debug=failure ++JvmOptions=-Djava.security.manager ++JvmOptions=-Djava.security.policy="%RED5_HOME%/conf/red5.policy" ++JvmOptions=-Dpython.home=lib
+set "EXECUTABLE=%RED5_HOME%\prunsrv.exe"
+set SERVICE_NAME=Red5
+set CLASSPATH="%RED5_HOME%\red5-service.jar;%RED5_HOME%\conf"
 
+rem Make sure prerequisite environment variables are set
+if not "%JAVA_HOME%" == "" goto gotJdkHome
+if not "%JRE_HOME%" == "" goto gotJreHome
+echo Neither the JAVA_HOME nor the JRE_HOME environment variable is defined
+echo Service will try to guess them from the registry.
+goto okJavaHome
+:gotJreHome
+if not exist "%JRE_HOME%\bin\java.exe" goto noJavaHome
+if not exist "%JRE_HOME%\bin\javaw.exe" goto noJavaHome
+goto okJavaHome
+:gotJdkHome
+if not exist "%JAVA_HOME%\jre\bin\java.exe" goto noJavaHome
+if not exist "%JAVA_HOME%\jre\bin\javaw.exe" goto noJavaHome
+if not exist "%JAVA_HOME%\bin\javac.exe" goto noJavaHome
+if not "%JRE_HOME%" == "" goto okJavaHome
+set "JRE_HOME=%JAVA_HOME%\jre"
+goto okJavaHome
+:noJavaHome
+echo The JAVA_HOME environment variable is not defined correctly
+echo This environment variable is needed to run this program
+echo NB: JAVA_HOME should point to a JDK not a JRE
+goto end
+:okJavaHome
+
+rem Set the server jvm from JAVA_HOME
+set "JVM=%JRE_HOME%\bin\server\jvm.dll"
+if exist "%JVM%" goto foundJvm
+rem Set the client jvm from JAVA_HOME
+set "JVM=%JRE_HOME%\bin\client\jvm.dll"
+if exist "%JVM%" goto foundJvm
+set JVM=auto
+:foundJvm
+echo Using JVM:              "%JVM%"
+
+echo Installing '%SERVICE_NAME%' service
+"%EXECUTABLE%" //IS//%SERVICE_NAME% ^
+    --Description "Red5 Media Server" ^
+    --DisplayName "Red5 Media Server" ^
+    --Install "%EXECUTABLE%" ^
+    --LogPath "%RED5_HOME%\logs" ^
+    --StdOutput "%RED5_HOME%\log\red5_service.log" ^
+    --StdError "%RED5_HOME%\log\stderr.log" ^
+    --Classpath "%CLASSPATH%" ^
+    --Jvm "%JVM%" ^
+    --StartMode jvm ^
+    --StartPath "%RED5_HOME%" ^
+    --StartClass org.red5.server.Bootstrap ^
+    --StartMethod main ^
+    --StopMode jvm ^
+    --StopPath "%RED5_HOME%" ^
+    --StopClass org.red5.server.Shutdown ^
+    --StopMethod main ^
+    --StopParams 9999;red5user;changeme ^
+    --JvmOptions "-Djavax.net.ssl.keyStore=%RED5_HOME%/conf/keystore.jmx;-Djavax.net.ssl.keyStorePassword=password;-Xverify:none;-XX:+TieredCompilation;-XX:+UseBiasedLocking;-XX:+UseStringCache;-XX:+UseParNewGC;-XX:InitialCodeCacheSize=8m;-XX:ReservedCodeCacheSize=32m;-Dorg.terracotta.quartz.skipUpdateCheck=true;-Dlogback.ContextSelector=org.red5.logging.LoggingContextSelector;-Dcatalina.useNaming=true;-Djava.security.debug=failure;-Djava.security.manager;-Djava.security.policy=%RED5_HOME%/conf/red5.policy;-Dpython.home=lib" ^
+    --JvmMs 256 ^
+    --JvmMx 768
+if not errorlevel 1 goto installed
+echo Failed installing '%SERVICE_NAME%' service
+goto end
+:installed
+echo The service '%SERVICE_NAME%' has been installed.
+
+:end
 ENDLOCAL
